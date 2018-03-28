9434e4683942c2ab8672531dc8a5dd464cc7df1a
Tim Schaeken <tschaeken@gmail.com>
2018-03-27 21:02:29 -0700

    updated with things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,36 +2,35 @@ var Char = {
     totalHealth: 300
 }
 
+var gameFunction = {
+    firstSwitch() {
+        $("#story-status-2").fadeIn(1000);
+    },
+    secondSwitch() {
+        $("#story-status-3").fadeIn(1000);
+    }
+}
 
-
-
-
-
+$("#object-container").on("click", "#weaponHammer", function(){
+    Char.attack = 130;
+    Char.criticalStrike = 0.17;
+    $("#story-status").fadeOut(1000, gameFunction.firstSwitch);
+})
+$("#object-container").on("click", "#weaponShovel", function(){
+    Char.attack = 110;
+    Char.criticalStrike = 0.24;
+    $("#story-status").fadeOut(1000, gameFunction.firstSwitch);
+})
+$("#object-container").on("click", "#weaponBillhook", function(){
+    Char.attack = 90;
+    Char.criticalStrike = 0.4;
+    $("#story-status").fadeOut(1000, gameFunction.firstSwitch);
+})
+$("#object-container").on("click", "#left", function(){
+    $("#story-status-2").fadeOut(1000, gameFunction.secondSwitch)
+})
 
 $(document).ready(function () {
     $('body').hide().fadeIn(1000);
 });
 
-
-
-function firstSwitch() {
-    $("#first-direction").fadeIn(1000);
-}
-
-
-
-$("#object-container").on("click", "#weaponHammer", function(){
-    Char.attack = 130;
-    Char.criticalStrike = 0.17;
-    $("#object-container").fadeOut(1000, firstSwitch);
-})
-$("#object-container").on("click", "#weaponShovel", function(){
-    Char.attack = 110;
-    Char.criticalStrike = 0.24;
-    $("#object-container").fadeOut(1000, firstSwitch);
-})
-$("#object-container").on("click", "#weaponBillhook", function(){
-    Char.attack = 90;
-    Char.criticalStrike = 0.4;
-    $("#object-container").fadeOut(1000, firstSwitch);
-})
